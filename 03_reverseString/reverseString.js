e862454b94dc6fda513ddf594cddb9dd5d17f004
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-24 13:15:45 -0700

    03_reverseString
    Code the function reverseString and passed all the
    tests from reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    for (let num = string.length -1; num >= 0 ; num--) {
+        reversed += string.charAt(num);
+    }
+    return reversed;
 };
 
+// console.log(reverseString("Hello there"));
 // Do not edit below this line
 module.exports = reverseString;
