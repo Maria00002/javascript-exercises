c7d927e0a096cb162c1d4db7c2c495b2cbe94859
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-24 16:34:23 -0700

    removeFromArray
    Write the function removeFromArray that passed all
     the tests from removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    console.log("Array is: " + arr);
+    console.log("Args are: " + args);
+    for (const arg of args) {
+        for(let i = 0; i < arr.length; i++) {
+            if (arr[i] === arg) {
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
+let output = removeFromArray([1,2,3,3,4,5],3,5);
+console.log(output);
 // Do not edit below this line
 module.exports = removeFromArray;
