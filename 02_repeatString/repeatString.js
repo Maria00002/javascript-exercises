cc2af7abe2e40001fba88099cfd901d913666249
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-24 12:48:38 -0700

    02_repeatString
    Code the function repeatString and passed all the
    tests from repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let returnedString = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+
+        for (let i = 0; i < num; i++){
+            returnedString += string;
+        }
+        return returnedString;
+    }
 
 };
+
+// console.log(repeatString("hey",3));
 
 // Do not edit below this line
 module.exports = repeatString;
