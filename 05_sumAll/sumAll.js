a90406acd3853eab48ea32fb4229dce3c0329cdb
Maria <100872877+Maria00002@users.noreply.github.com>
2025-04-25 09:52:35 -0700

    05_sumAll
    Code and passed all the tests:
    ✓ sums numbers within the range
    ✓ works with large numbers
    ✓ works with larger number first
    ✓ returns ERROR with negative numbers
    ✓ returns ERROR with non-integer parameters
    ✓ returns ERROR with non-number parameters
    ✓ returns ERROR with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    if (Number.isInteger(startNum) && Number.isInteger(endNum)) {
+        if (startNum > endNum) {
+            let num = startNum;
+            startNum = endNum;
+            endNum = num;
+            sum = calculateSum (startNum, endNum);
+        }
+        else if (startNum <= endNum) {
+            if (startNum < 0 || endNum < 0) {
+                return "ERROR";
+            }
+            else {
+                sum = calculateSum (startNum, endNum);
+            }
+        }
+        
+        return sum;    
+    } else {
+        return "ERROR";
+    }
+  
 };
+
+function calculateSum (startNum, endNum) {
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
