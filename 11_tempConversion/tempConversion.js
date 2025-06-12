4d9a05ca11d7f5ba48d990329ecdda1c9763fd8a
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-12 12:44:07 -0700

    11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // fahrenheit to celsius
+  // (temp - 32 ) * 5 / 9
+  let celsius;
+  let rounded;
+  celsius = (temp - 32) * 5 / 9;
+  // round to one decimal place
+  return rounded = Math.round(celsius * 10) /10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // celsius to fahrenheit
+  // (temp * 9 / 5 + 32)
+  let fahrenheit;
+  let rounded;
+  fahrenheit = (temp * 9 / 5) + 32;
+  rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
+  // round to one decimal place
 };
 
 // Do not edit below this line
