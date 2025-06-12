b28f622fc93ef20eb94a44b7fc2e0b17beba9115
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-12 12:26:55 -0700

    09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let newArr = [];
+    if (firstNum > lastNum) {
+        let num = firstNum;
+        firstNum = lastNum;
+        lastNum = num;
+    }
+    if (!Number.isInteger(firstNum) || !Number.isInteger(lastNum)) {
+        return "ERROR";
+    }
+    if ((firstNum < 0 || lastNum < 0)){
+        return "ERROR";
+    }
+    for (let i = firstNum; i <= lastNum; i++) {
+        newArr.push(i);
+    }
+    return newArr.reduce((total, currentValue)=>total+currentValue, 0);
 };
 
 // Do not edit below this line
