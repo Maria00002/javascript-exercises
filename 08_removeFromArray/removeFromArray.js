cb9b303227c763bcfbf12e6df87342ad2622cefb
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-12 11:46:40 -0700

    Exercise 08 - removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        let contain = arr.includes(arg);
+        while (contain) {
+            let index = arr.indexOf(arg);
+            arr.splice(index,1);
+            contain = arr.includes(arg);
+        }
+    }    
+    return arr;
 };
 
 // Do not edit below this line
