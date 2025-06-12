32b320ac297fac4e23312ae8db5642eb5f19beb9
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-12 12:32:29 -0700

    Exercise 10 - leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Leap years are years divisible by four, but not devisible by 100
+    // unless they are divisible by 400 
+    if (year % 4 == 0 && year % 400 == 0) {
+        return true;
+    } else if (year % 4 == 0 && year % 100 ==0) {
+        return false;
+    }  else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
