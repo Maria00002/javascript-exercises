8b07361765d6d05158796b65c6677b8c768a4bb8
Maria <100872877+Maria00002@users.noreply.github.com>
2025-06-12 11:25:59 -0700

    07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedArr = [];
+    let arr = string.split("");
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversedArr.push(arr[i]);
+        console.log(reversedArr); 
+    }
+    let returnedString = reversedArr.join("");
+    return returnedString;
 
 };
 
