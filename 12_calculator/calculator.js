b0792c17ff417dfee429e2d120fa73f63a8d720c
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-23 12:48:06 -0700

    12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for (let key in arr) {
+    sum += arr[key];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let result = 1;
+  for (let key in arr) {
+    result *= arr[key];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+  let result = 1;
+  for (let i = 1; i <= b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	if (num == 0 ) {
+    result = 1;
+  } else {
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
