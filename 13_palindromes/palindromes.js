54a9aed8ad5718a276126512db98bab7f7ca1473
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-23 16:22:54 -0700

    13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arr = str.toLowerCase().split(""); //arr is now lowercased str
+    let newArr = [];
+    let reverse = [];
+    for (let char of arr) {
+        if (isChar(char) || Number.isInteger(Number(char))) {
+            newArr.push(char);
+        }
+    }
 
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i] === " ") {
+            newArr.splice(i,1);
+        }
+    }
+
+    let newStr = newArr.join("");
+    console.log(newStr); //
+    reverse = newArr.reverse();
+    let reverseStr = reverse.join("");
+    console.log(reverseStr); //
+    console.log(newStr.includes(reverseStr)); // 
+    return newStr.includes(reverseStr);
 };
+
+function isChar(str) {
+    return str.match(/[a-z]/i);
+}
+
+// palindromes("ZZZZ car, a man, a maracaz");
 
 // Do not edit below this line
 module.exports = palindromes;
