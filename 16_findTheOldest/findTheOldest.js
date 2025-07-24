97231d981533aaa2a02fcbb7346a3ea5575dab40
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-23 18:17:41 -0700

    16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // console.log();
+    let compare = [];
+    for (let key in people) {
+        if (people[key]["yearOfDeath"] === undefined) {
+            people[key]["yearOfDeath"] = new Date().getFullYear();
+        } 
+        compare.push(people[key]["yearOfDeath"] - people[key]["yearOfBirth"]);
+    }
+    let oldest = compare[0];
+    let index = 0;
+    for (let i = 0; i < compare.length; i++) {
+        if (oldest < compare[i]) {
+            oldest = compare[i];
+            index = i;
+        }
+    }
+    console.log(people[index]); //
+    return people[index];
+    
+    
 
 };
+
+
+// findTheOldest(people);
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
