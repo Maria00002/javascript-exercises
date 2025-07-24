ca3b5892c0b9163037fbfaabcec5c67191d8283e
Maria <100872877+Maria00002@users.noreply.github.com>
2025-07-23 17:46:56 -0700

    15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    for (key in books) {
+        arr.push(books[key]["title"]);
+    }
+    return arr;
 };
+
+// getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
